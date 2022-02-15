5cbcd832975dad36f20805f9e9529161f8a2eb36
brianmirko <brian.mirko.rm@gmail.com>
2022-02-15 01:23:52 -0400

    create new react app Task To Do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { TaskRow } from "./components/TaskRow";
+import { TaskBanner } from "./components/TaskBanner";
+import { TaskCreator } from "./components/TaskCreator";
+import { VisibilityControl } from "./components/VisibilityControl";
 
 function App() {
+  const [userName, setUserName] = useState("BrianMirko");
+  const [taskItems, setTaskItems] = useState([
+    { name: "Task One", done: false },
+    { name: "Task Two", done: false },
+    { name: "Task Three", done: true },
+    { name: "Task Four", done: false },
+  ]);
+  const [showComplete, setShowComplete] = useState(true);
+
+  useEffect(() => {
+    let data = localStorage.getItem("tasks");
+    if (data !== null) {
+      setTaskItems(JSON.parse(data));
+    } else {
+      setUserName("BrianMirko Example");
+      setTaskItems([
+        { name: "Task One Example", done: false },
+        { name: "Task Two Example", done: false },
+        { name: "Task Three Example", done: true },
+        { name: "Task Four Example", done: false },
+      ]);
+      setShowComplete(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskItems));
+  }, [taskItems]);
+
+  const createNewTask = (taskName) => {
+    if (!taskItems.find((t) => t.name === taskName)) {
+      setTaskItems([...taskItems, { name: taskName, done: false }]);
+    }
+  };
+
+  const toggleTask = (task) => {
+    return setTaskItems(
+      taskItems.map((t) => {
+        return t.name === task.name ? { ...t, done: !t.done } : t;
+      })
+    );
+  };
+
+  const taskTableRows = (doneValue) => {
+    return taskItems
+      .filter((task) => task.done === doneValue)
+      .map((task) => {
+        return <TaskRow task={task} key={task.name} toggleTask={toggleTask} />;
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TaskBanner userName={userName} taskItems={taskItems} />
+      <TaskCreator callback={createNewTask} />
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>{taskTableRows(false)}</tbody>
+      </table>
+      <div className="bg-secondary-text-white text-center p-2">
+        <VisibilityControl
+          description="Completed Task"
+          isChecked={showComplete}
+          callback={(checked) => setShowComplete(checked)}
+        />
+      </div>
+      {showComplete && (
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{taskTableRows(true)}</tbody>
+        </table>
+      )}
     </div>
   );
 }
---
+++ src/components/TaskBanner.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const TaskBanner = (props) => {
+  return (
+    <h4 className="bg-primary text-white text-center p-4">
+      {props.userName}'s Task App (
+      {props.taskItems.filter((t) => !t.done).length} Task to do)
+    </h4>
+  );
+};
